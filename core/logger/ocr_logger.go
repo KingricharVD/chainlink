aa3ddb953ce487aa277c7e767d28c75994a4fb5b
spooktheducks <spooktheducks@protonmail.com>
2020-10-01 19:44:44 -0500

    Add Trace to OCR logger

--- core/logger/ocr_logger.go
+++ core/logger/ocr_logger.go
@@ -1,5 +1,9 @@
 package logger
 
+import (
+	ocrtypes "github.com/smartcontractkit/offchain-reporting/lib/offchainreporting/types"
+)
+
 // ocrLogger is an implemenation of the Logger interface for OCR
 // See: https://github.com/smartcontractkit/offchain-reporting-design/blob/master/prototype/offchainreporting/types/logger.go#L3
 type ocrLogger struct {
@@ -12,25 +16,30 @@ func NewOCRLogger(internal *Logger) OCRLogger {
 	}
 }
 
-func (ol *ocrLogger) Debug(msg string, fields LogFields) {
+func (ol *ocrLogger) Debug(msg string, fields ocrtypes.LogFields) {
 	ol.internal.Debugw(msg, toKeysAndValues(fields))
 }
 
-func (ol *ocrLogger) Info(msg string, fields LogFields) {
+func (ol *ocrLogger) Info(msg string, fields ocrtypes.LogFields) {
 	ol.internal.Infow(msg, toKeysAndValues(fields))
 }
 
-func (ol *ocrLogger) Warn(msg string, fields LogFields) {
+func (ol *ocrLogger) Warn(msg string, fields ocrtypes.LogFields) {
 	ol.internal.Warnw(msg, toKeysAndValues(fields))
 }
 
-func (ol *ocrLogger) Error(msg string, fields LogFields) {
+func (ol *ocrLogger) Error(msg string, fields ocrtypes.LogFields) {
 	ol.internal.Errorw(msg, toKeysAndValues(fields))
+}
+
+func (ol *ocrLogger) Trace(msg string, fields ocrtypes.LogFields) {
+	// @@TODO ???
+	ol.internal.Debugw(msg, toKeysAndValues(fields))
 }
 
 // Helpers
 
-func toKeysAndValues(fields LogFields) []interface{} {
+func toKeysAndValues(fields ocrtypes.LogFields) []interface{} {
 	out := []interface{}{}
 	for key, val := range fields {
 		out = append(out, key, val)
@@ -45,8 +54,9 @@ func toKeysAndValues(fields LogFields) []interface{} {
 type LogFields map[string]interface{}
 
 type OCRLogger interface {
-	Debug(msg string, fields LogFields)
-	Info(msg string, fields LogFields)
-	Warn(msg string, fields LogFields)
-	Error(msg string, fields LogFields)
+	Debug(msg string, fields ocrtypes.LogFields)
+	Info(msg string, fields ocrtypes.LogFields)
+	Warn(msg string, fields ocrtypes.LogFields)
+	Error(msg string, fields ocrtypes.LogFields)
+	Trace(msg string, fields ocrtypes.LogFields)
 }
